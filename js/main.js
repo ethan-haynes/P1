4f3f7792a54f3417965d1f03d31d3bf4cab404d5
ethan <ehaynes@g.harvard.edu>
2016-09-07 22:10:00 -0500

    focus on landing on page load

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@
 
     //Document ready
     $(function() {
+        //focus on page load
+        $('.landing').focus();
+
         $( window ).scroll(function() {
             //ease to main page
             //make navbar fixed at top
