859761f23cfb7ee4174ba5e060432d4b187a7fce
ethan <ehaynes@g.harvard.edu>
2016-09-15 06:58:45 -0500

    added to README. fixed tag validator issue

--- README.md
+++ README.md
@@ -3,13 +3,14 @@ P1
 ## URL
 http://p1.ehay.me
 ## Description
-This is a single page website
+This is a single page website as a portfolio for all of my projects for DWA15
 ## Link to demo
-TODO
+https://youtu.be/oqjDeczH-5o
 ## Usage
-TODO
+When using this page you will first land on the welcome spot of the main page. If you scroll down it will autoscroll and bring you to the main content of the page inside the body. This is where links to later projects are stored, as well as a little information about me. When you scroll back up, past the navigation, it will bring you to the main landing spot again.
 ## Dependencies
 Frameworks: Bootstrap
+Libraries: jQuery, jquery-ui
 GoogleFonts: Ubuntu
 
 ## License
--- index.php
+++ index.php
@@ -11,80 +11,89 @@
             <link rel="stylesheet" href="css/jquery-ui.css">
         </head>
     <body>
-        <div class="row"><div class="col-xs-12 col-md-12 landing"> <div>Welcome</div> <span><div>this is project 1</div> </span></div></div>
+        <!-- main landing -->
+        <div class="row"><div class="col-xs-12 col-md-12 landing"> <div>Welcome</div> <span>this is project 1</span></div></div>
         <div class="row">
-        <nav class="col-xs-12 col-md-12 navbar navbar-default navbar-static-top">
-            <div class="container">
-                <ul class="nav nav-pills">
-                    <li role="presentation"><a href="#">Project 1</a></li>
-                    <li role="presentation"><a href="#">Project 2</a></li>
-                    <li role="presentation"><a href="#">Project 3</a></li>
-                    <li role="presentation"><a href="#">Project 4</a></li>
-                </ul>
-            </div>
-        </nav>
-        <br>
-        <br>
-        <br>
-    </div>
+            <nav class="col-xs-12 col-md-12 navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <ul class="nav nav-pills">
+                        <li role="presentation"><a href="#">Project 1</a></li>
+                        <li role="presentation"><a href="#">Project 2</a></li>
+                        <li role="presentation"><a href="#">Project 3</a></li>
+                        <li role="presentation"><a href="#">Project 4</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <br>
+            <br>
+            <br>
+        </div>
         <div class="row">
-        <div class="col-xs-1 col-md-2"></div>
+            <!-- main column left -->
+            <div class="col-xs-1 col-md-2"></div>
+            <!-- main column center -->
             <div class="col-md-8 container">
                 <div class="content">
                     <div class="title">Ethan Haynes</div>
-                        <button type="button" name="button">button</button>
-                        <img src="imgs/head-small.jpg" alt="portrait"/>
-                        <div class="row">
-                                <span class="col-xs-6 col-md-6 ">
-                                    <a href="#"><div>P1 [live]</div></a>
-                                    <a href="#"><div>P1 [github]</div></a>
-                                    <a href="#"><div>P2 [live]</div></a>
-                                    <a href="#"><div>P2 [github]</div></a>
-                                </span>
-
-                                <div></div>
-                                
-                                <span class="col-xs-6 col-md-6 ">
-                                    <a href="#"><div>P3 [live]</div></a>
-                                    <a href="#"><div>P3 [github]</div></a>
-                                    <a href="#"><div>P4 [live]</div></a>
-                                    <a href="#"><div>P4 [github]</div></a>
-                                </span>
+                    <button type="button" name="button">button</button>
+                    <img src="imgs/head-small.jpg" alt="portrait"/>
+                    <div class="row">
+                        <!-- column left -->
+                        <div class="col-md-2"></div>
+                        <!-- column center -->
+                        <div class="col-xs-3 col-md-2">
+                            <a href="#">P1 [live]</a>
+                            <a href="#">P1 [github]</a>
                         </div>
+                        <div class="col-xs-3 col-md-2 ">
+                            <a href="#">P2 [live]</a>
+                            <a href="#">P2 [github]</a>
+                        </div>
+
+                        <div class="col-xs-3 col-md-2 ">
+                            <a href="#">P3 [live]</a>
+                            <a href="#">P3 [github]</a>
+                        </div>
+                        <div class="col-xs-3 col-md-2">
+                            <a href="#">P4 [live]</a>
+                            <a href="#">P4 [github]</a>
+                        </div>
+                        <!-- column right -->
+                        <div class="col-md-2"></div>
                     </div>
-                        <ul>
-                            <hr>
-                            <li>
+                </div>
+                    <ul>
+                        <li>
+                            <div class="row">
                                 <div class="row">
-                                    <div class="row">
-                                        <div class="col-xs-2 col-md-1"></div>
-                                        <h2 class="col-xs-10 col-md-9 article-title text-left">Questions</h2>
+                                    <div class="col-xs-2 col-md-1"></div>
+                                    <h2 class="col-xs-10 col-md-9 article-title text-left">Questions</h2>
+                                </div>
+                                <div class="col-xs-4 col-md-2 circle">
+                                    <div class="v-line"></div>
+                                </div>
+                                <div class="col-xs-8 col-md-10 inner">
+                                    <p>What is your name as listed with HES and what you prefer to be called (if different than what is listed)?</p>
+
+                                    <div>Name: Ethan Haynes</div>
+
+                                    <p>What do you do?</p>
+
+                                    <div>I am a software engineer at an IT consulting firm in Dallas, TX. I mainly work with java in my everyday job. I do some front-end work with JSP, CSS, XML, Javascript, but not much. In my free time I work full-stack on far too many side projects.</div>
+
+                                    <p>Why are you taking this course?</p>
+                                    <div>I am a degree candidate here at Harvard University Extension School, and this is one of the last courses I need to fulfill my degree requirement. Secondly, I took this course because I am interested in learning more about developing web applications in different languages and frameworks. I feel it is important to sharpen my skills in as many areas as possible to stay relevant.</div>
+                                    <p>Anything else you'd like to share?</p>
+                                    <div>
+                                        One of the more interesting projects I worked on was developing an application in Java that takes a dataset of city coordinates and determines an optimal tour of minimal distance to solve the Traveling Salesman Problem. This was accomplished using Genetic Algorithms and Crossover and Mutation Operators. Neat stuff.
                                     </div>
-                                    <div class="col-xs-4 col-md-2 circle">
-                                        <div class="v-line"></div>
-                                    </div>
-                                    <div class="col-xs-8 col-md-10 inner">
-                                        <p>What is your name as listed with HES and what you prefer to be called (if different than what is listed)?</p>
-
-                                        <div>Name: Ethan Haynes</div>
-
-                                        <p>What do you do?</p>
-
-                                        <div>I am a software engineer at an IT consulting firm in Dallas, TX. I mainly work with java in my everyday job. I do some front-end work with JSP, CSS, XML, Javascript, but not much. In my free time I work full-stack on far too many side projects.</div>
-
-                                        <p>Why are you taking this course?</p>
-                                        <div>I am a degree candidate here at Harvard University Extension School, and this is one of the last courses I need to fulfill my degree requirement. Secondly, I took this course because I am interested in learning more about developing web applications in different languages and frameworks. I feel it is important to sharpen my skills in as many areas as possible to stay relevant.</div>
-                                        <p>Anything else you'd like to share?</p>
-                                        <div>
-                                            One of the more interesting projects I worked on was developing an application in Java that takes a dataset of city coordinates and determines an optimal tour of minimal distance to solve the Traveling Salesman Problem. This was accomplished using Genetic Algorithms and Crossover and Mutation Operators. Neat stuff.
-                                        </div>
-                                        <div>
-                                            I am currently building a desktop game with the Electron framework. It is javascript, CSS, and HTML on the front-end and Node.js on the backend. The game map is an isometric grid with sprites generated by HTML5 canvas.
-                                        </div>
+                                    <div>
+                                        I am currently building a desktop game with the Electron framework. It is javascript, CSS, and HTML on the front-end and Node.js on the backend. The game map is an isometric grid with sprites generated by HTML5 canvas.
                                     </div>
                                 </div>
-                            </li>
-                        </ul>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             <div class="col-xs-1 col-md-2"></div>
         </div>
